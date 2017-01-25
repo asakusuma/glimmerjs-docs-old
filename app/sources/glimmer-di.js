26e05a170720244c75ac6a0dfda191215ea55697
asakusuma <asa.kusuma@gmail.com>
2017-01-25 09:48:23 -0800

    Add dummy data source

---
+++ app/sources/glimmer-di.js
@@ -0,0 +1,15 @@
+export default {
+  "name": "glimmer-di",
+  "modules": [
+    {
+      "name": "container",
+      "source": "https://github.com/glimmerjs/glimmer-di/blob/master/src/container.ts",
+      "type": "class",
+      "methods": [
+        {
+          "name": "constructor"
+        }
+      ]
+    }
+  ]
+};
